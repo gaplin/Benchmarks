01fdeaf18b00bb3dcaae8aef0939d4dfa4a0c815
gaplin <gaplinn@gmail.com>
2024-09-28 15:56:11 +0200

    Benchmark for multiple toList() calls

---
+++ EnumerableToList/Benchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace EnumerableToList;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private readonly List<int> _items = Enumerable.Range(1, 1_000_000).ToList();
+
+    [Benchmark(Baseline = true)]
+    public List<int> JustEnumerable()
+    {
+        IEnumerable<int> odds = _items.Where(x => x % 2 == 1);
+
+        return odds.Select(x => x * x).ToList();
+    }
+
+    [Benchmark]
+    public List<int> DoubleToList()
+    {
+        List<int> odds = _items.Where(x => x % 2 == 1).ToList();
+
+        return odds.Select(x => x * x).ToList();
+    }
+}
