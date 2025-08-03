b28ef441a88e511d5342683c054c0ae7321fef76
gaplin <50521366+gaplin@users.noreply.github.com>
2025-08-03 12:03:01 +0200

    Benchmark for linq chaining

---
+++ LinqChaining/Benchmarks.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LinqChaining;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private int[] _nums = null!;
+    private readonly Random _random = new(5);
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _nums = [.. Enumerable.Range(0, 1_000_000).Select(x => _random.Next(1_000_000_000))];
+    }
+
+    [Benchmark(Baseline = true)]
+    public long NoLinq()
+    {
+        long result = 0;
+        foreach (var num in _nums)
+        {
+            if (num % 2 == 0 && num > 10 && num < 5_500_000 && num % 3 == 0 && num % 5 == 2)
+            {
+                result += num;
+            }
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public long Linq()
+    {
+        long result = 0;
+        foreach (var num in _nums
+            .Where(x => x % 2 == 0)
+            .Where(x => x > 10)
+            .Where(x => x < 5_500_000)
+            .Where(x => x % 3 == 0)
+            .Where(x => x % 5 == 2))
+        {
+            result += num;
+        }
+        return result;
+    }
+}
---
+++ LinqChaining/README.md
@@ -0,0 +1,13 @@
+﻿# Results
+
+BenchmarkDotNet v0.15.2, Windows 11 (10.0.26100.4652/24H2/2024Update/HudsonValley)\
+12th Gen Intel Core i7-12650H 2.30GHz, 1 CPU, 16 logical and 10 physical cores\
+.NET SDK 9.0.302\
+  [Host]     : .NET 9.0.7 (9.0.725.31616), X64 RyuJIT AVX2\
+  DefaultJob : .NET 9.0.7 (9.0.725.31616), X64 RyuJIT AVX2
+
+
+| Method | Mean     | Error     | StdDev    | Ratio | RatioSD | Allocated | Alloc Ratio |
+|------- |---------:|----------:|----------:|------:|--------:|----------:|------------:|
+| NoLinq | 3.590 ms | 0.0362 ms | 0.0321 ms |  1.00 |    0.01 |         - |          NA |
+| Linq   | 9.911 ms | 0.1929 ms | 0.2369 ms |  2.76 |    0.07 |     624 B |          NA |
