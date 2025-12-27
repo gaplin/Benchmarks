bd61b8901e769ed97f36e0d50bd17f3630dfdf21
gaplin <50521366+gaplin@users.noreply.github.com>
2025-12-27 23:24:19 +0100

    Json single prop deserialization benchmark

---
+++ JsonOps/Benchmarks.cs
@@ -0,0 +1,138 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonOps;
+
+[MemoryDiagnoser(displayGenColumns: true)]
+public class Benchmarks
+{
+    private readonly Random _rand = new(5555);
+    private string _json = "";
+
+    [Params(100, 100_000)]
+    public int Properties { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var jsonInputBuild = new StringBuilder("{");
+
+        for (int i = Properties; i > 1; --i)
+        {
+            jsonInputBuild.Append($"\"property_{i}\":{_rand.Next()},");
+        }
+        jsonInputBuild.Append($"\"property_1\":{_rand.Next()}}}");
+        _json = jsonInputBuild.ToString();
+    }
+
+    [Benchmark(Baseline = true)]
+    public Target JsonSerializer_Deserlialize()
+    {
+        return JsonSerializer.Deserialize<Target>(_json)!;
+    }
+
+    [Benchmark]
+    public Target JsonReader_Buffered()
+    {
+        var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(_json));
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.PropertyName && reader.ValueTextEquals("property_1"u8))
+            {
+                reader.Read();
+                return new Target { Value = reader.GetInt32() };
+            }
+        }
+
+        throw new InvalidOperationException("Invalid input");
+    }
+
+    [Benchmark]
+    public Target JsonReader_Streamed()
+    {
+        Span<byte> buffer = stackalloc byte[1000];
+        var jsonSpan = _json.AsSpan();
+        int chunkSize = 1000;
+        var startIncl = 0;
+        int endExcl = chunkSize;
+        var writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
+        startIncl += writtenBytes;
+        endExcl += writtenBytes;
+        var reader = new Utf8JsonReader(buffer, isFinalBlock: false, state: default);
+        int bufferEnd = 1000;
+        while (reader.TokenType != JsonTokenType.PropertyName || !reader.ValueTextEquals("property_1"u8))
+        {
+            if (!reader.Read())
+            {
+                int leftOverLength = 0;
+                if (reader.BytesConsumed < buffer.Length)
+                {
+                    ReadOnlySpan<byte> leftover = buffer[(int)reader.BytesConsumed..];
+                    leftOverLength = leftover.Length;
+                    if (leftOverLength == buffer.Length)
+                    {
+                        throw new InvalidOperationException("Too short");
+                    }
+
+                    leftover.CopyTo(buffer);
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000 - leftOverLength);
+                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer[leftOverLength..]);
+                    startIncl += writtenBytes;
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
+                }
+                else
+                {
+                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
+                    startIncl += writtenBytes;
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
+                }
+                bufferEnd = leftOverLength + writtenBytes;
+                reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: startIncl == jsonSpan.Length, reader.CurrentState);
+            }
+        }
+
+        while (reader.TokenType != JsonTokenType.Number)
+        {
+            if (!reader.Read())
+            {
+                int leftOverLength = 0;
+                if (reader.BytesConsumed < buffer.Length)
+                {
+                    ReadOnlySpan<byte> leftover = buffer[(int)reader.BytesConsumed..];
+                    leftOverLength = leftover.Length;
+                    if (leftOverLength == buffer.Length)
+                    {
+                        throw new InvalidOperationException("Too short");
+                    }
+
+                    leftover.CopyTo(buffer);
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000 - leftOverLength);
+                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer[leftOverLength..]);
+                    startIncl += writtenBytes;
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
+                }
+                else
+                {
+                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
+                    startIncl += writtenBytes;
+                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
+                }
+                bufferEnd = leftOverLength + writtenBytes;
+                reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: startIncl == jsonSpan.Length, reader.CurrentState);
+            }
+        }
+        return new Target
+        {
+            Value = reader.GetInt32()
+        };
+        throw new InvalidOperationException("Invalid input");
+    }
+
+    public sealed class Target
+    {
+        [JsonPropertyName("property_1")]
+        public int Value { get; set; }
+    }
+}
---
+++ JsonOps/README.md
@@ -0,0 +1,18 @@
+﻿# Results
+
+BenchmarkDotNet v0.15.8, Windows 11 (10.0.26200.7462/25H2/2025Update/HudsonValley2)\
+AMD Ryzen 9 7945HX with Radeon Graphics 2.50GHz, 1 CPU, 32 logical and 16 physical cores\
+.NET SDK 10.0.101\
+  [Host]     : .NET 10.0.1 (10.0.1, 10.0.125.57005), X64 RyuJIT x86-64-v4\
+  DefaultJob : .NET 10.0.1 (10.0.1, 10.0.125.57005), X64 RyuJIT x86-64-v4
+
+
+| Method                      | Properties | Mean          | Error      | StdDev     | Ratio | Gen0     | Gen1     | Gen2     | Allocated | Alloc Ratio |
+|---------------------------- |----------- |--------------:|-----------:|-----------:|------:|---------:|---------:|---------:|----------:|------------:|
+| JsonSerializer_Deserlialize | 100        |      8.086 us |  0.0768 us |  0.0718 us |  1.00 |   0.0763 |        - |        - |    1424 B |        1.00 |
+| JsonReader_Buffered         | 100        |      2.058 us |  0.0189 us |  0.0167 us |  0.25 |   0.1488 |        - |        - |    2496 B |        1.75 |
+| JsonReader_Streamed         | 100        |      2.332 us |  0.0200 us |  0.0187 us |  0.29 |        - |        - |        - |      24 B |        0.02 |
+|                             |            |               |            |            |       |          |          |          |           |             |
+| JsonSerializer_Deserlialize | 100000     | 18,842.868 us | 98.5991 us | 82.3347 us |  1.00 | 218.7500 |        - |        - | 3997424 B |       1.000 |
+| JsonReader_Buffered         | 100000     |  2,345.837 us | 11.7795 us | 10.4423 us |  0.12 | 324.2188 | 324.2188 | 324.2188 | 2737490 B |       0.685 |
+| JsonReader_Streamed         | 100000     |  2,643.738 us | 20.5112 us | 19.1862 us |  0.14 |        - |        - |        - |      24 B |       0.000 |
