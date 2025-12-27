a346cfa928b86d382b9ada159d6032afe3256943
gaplin <50521366+gaplin@users.noreply.github.com>
2025-12-27 23:37:13 +0100

    Fixed streamed version for small input + small refactoring

--- JsonOps/Benchmarks.cs
+++ JsonOps/Benchmarks.cs
@@ -54,42 +54,19 @@ public class Benchmarks
     {
         Span<byte> buffer = stackalloc byte[1000];
         var jsonSpan = _json.AsSpan();
-        int chunkSize = 1000;
+        const int chunkSize = 1000;
         var startIncl = 0;
-        int endExcl = chunkSize;
+        int endExcl = Math.Min(chunkSize, jsonSpan.Length);
         var writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
         startIncl += writtenBytes;
         endExcl += writtenBytes;
-        var reader = new Utf8JsonReader(buffer, isFinalBlock: false, state: default);
-        int bufferEnd = 1000;
+        int bufferEnd = writtenBytes;
+        var reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: false, state: default);
         while (reader.TokenType != JsonTokenType.PropertyName || !reader.ValueTextEquals("property_1"u8))
         {
             if (!reader.Read())
             {
-                int leftOverLength = 0;
-                if (reader.BytesConsumed < buffer.Length)
-                {
-                    ReadOnlySpan<byte> leftover = buffer[(int)reader.BytesConsumed..];
-                    leftOverLength = leftover.Length;
-                    if (leftOverLength == buffer.Length)
-                    {
-                        throw new InvalidOperationException("Too short");
-                    }
-
-                    leftover.CopyTo(buffer);
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000 - leftOverLength);
-                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer[leftOverLength..]);
-                    startIncl += writtenBytes;
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
-                }
-                else
-                {
-                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
-                    startIncl += writtenBytes;
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
-                }
-                bufferEnd = leftOverLength + writtenBytes;
-                reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: startIncl == jsonSpan.Length, reader.CurrentState);
+                ReadMoreText(buffer, jsonSpan, chunkSize, ref startIncl, ref endExcl, out writtenBytes, out bufferEnd, ref reader);
             }
         }
 
@@ -97,30 +74,7 @@ public class Benchmarks
         {
             if (!reader.Read())
             {
-                int leftOverLength = 0;
-                if (reader.BytesConsumed < buffer.Length)
-                {
-                    ReadOnlySpan<byte> leftover = buffer[(int)reader.BytesConsumed..];
-                    leftOverLength = leftover.Length;
-                    if (leftOverLength == buffer.Length)
-                    {
-                        throw new InvalidOperationException("Too short");
-                    }
-
-                    leftover.CopyTo(buffer);
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000 - leftOverLength);
-                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer[leftOverLength..]);
-                    startIncl += writtenBytes;
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
-                }
-                else
-                {
-                    writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
-                    startIncl += writtenBytes;
-                    endExcl = Math.Min(jsonSpan.Length, startIncl + 1_000);
-                }
-                bufferEnd = leftOverLength + writtenBytes;
-                reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: startIncl == jsonSpan.Length, reader.CurrentState);
+                ReadMoreText(buffer, jsonSpan, chunkSize, ref startIncl, ref endExcl, out writtenBytes, out bufferEnd, ref reader);
             }
         }
         return new Target
@@ -128,6 +82,34 @@ public class Benchmarks
             Value = reader.GetInt32()
         };
         throw new InvalidOperationException("Invalid input");
+    }
+
+    private static void ReadMoreText(Span<byte> buffer, ReadOnlySpan<char> jsonSpan, int chunkSize, ref int startIncl, ref int endExcl, out int writtenBytes, out int bufferEnd, ref Utf8JsonReader reader)
+    {
+        int leftOverLength = 0;
+        if (reader.BytesConsumed < buffer.Length)
+        {
+            ReadOnlySpan<byte> leftover = buffer[(int)reader.BytesConsumed..];
+            leftOverLength = leftover.Length;
+            if (leftOverLength == buffer.Length)
+            {
+                throw new InvalidOperationException("Too short");
+            }
+
+            leftover.CopyTo(buffer);
+            endExcl = Math.Min(jsonSpan.Length, startIncl + chunkSize - leftOverLength);
+            writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer[leftOverLength..]);
+            startIncl += writtenBytes;
+            endExcl = Math.Min(jsonSpan.Length, startIncl + chunkSize);
+        }
+        else
+        {
+            writtenBytes = Encoding.UTF8.GetBytes(jsonSpan[startIncl..endExcl], buffer);
+            startIncl += writtenBytes;
+            endExcl = Math.Min(jsonSpan.Length, startIncl + chunkSize);
+        }
+        bufferEnd = leftOverLength + writtenBytes;
+        reader = new Utf8JsonReader(buffer[..bufferEnd], isFinalBlock: startIncl == jsonSpan.Length, reader.CurrentState);
     }
 
     public sealed class Target
